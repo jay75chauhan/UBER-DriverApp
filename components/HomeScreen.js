a839bdcd5d6b378d02392bdee924fb6550db4350
jay75chauhan <chauhan75jayk@gmail.com>
2021-08-13 16:03:57 +0530

    setting up driver

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { GOOGLE_MAP_KEY } from "@env";
 import MapView from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
+import MapViewDirections from "react-native-maps-directions";
+
+const origin = { latitude: 37.3318456, longitude: -122.0296002 };
+const destination = { latitude: 37.771707, longitude: -122.4053769 };
 
 const HomeScreen = () => {
   return (
@@ -15,7 +20,16 @@ const HomeScreen = () => {
           longitudeDelta: 0.0421,
         }}
         mapType="mutedStandard"
-      />
+      >
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAP_KEY}
+          lineDashPattern={[0]}
+          strokeColor="black"
+          strokeWidth={5}
+        />
+      </MapView>
     </View>
   );
 };
